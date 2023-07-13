718491840cd55fac8e23e8c2414c430fb0209b2d
Matan Eshed <meshed72@gmail.com>
2023-07-13 22:35:14 +0300

    comment models out

--- foraging_task_app/models.py
+++ foraging_task_app/models.py
@@ -1,20 +1,20 @@
-from datetime import timezone
-from django.db import models
+# from datetime import timezone
+# from django.db import models
 
-class Subjects(models.Model):
-    subject_id = models.CharField(max_length=30)
-    start_time = models.BigIntegerField(default='NA')
+# class Subjects(models.Model):
+#     subject_id = models.CharField(max_length=30)
+#     start_time = models.BigIntegerField(default='NA')
     
-    class Meta:
-        db_table  = 'subjects'
+#     class Meta:
+#         db_table  = 'subjects'
         
-class Task_clicks(models.Model):
-    subject_id = models.CharField(max_length=30)
-    click_time = models.BigIntegerField()
-    patch_number = models.IntegerField()
-    x = models.IntegerField()
-    y = models.IntegerField()
-    is_ripe = models.BooleanField()
+# class Task_clicks(models.Model):
+#     subject_id = models.CharField(max_length=30)
+#     click_time = models.BigIntegerField()
+#     patch_number = models.IntegerField()
+#     x = models.IntegerField()
+#     y = models.IntegerField()
+#     is_ripe = models.BooleanField()
     
-    class Meta:
-        db_table  = 'task_clicks'
+#     class Meta:
+#         db_table  = 'task_clicks'
