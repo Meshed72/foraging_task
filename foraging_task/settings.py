1cc793a6c2c6b8d241f7d9c2ae21ae41902807f2
Matan Eshed <meshed72@gmail.com>
2023-07-14 11:15:40 +0300

    .

--- foraging_task/settings.py
+++ foraging_task/settings.py
@@ -80,6 +80,18 @@ WSGI_APPLICATION = 'foraging_task.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+        'default': {
+            'ENGINE': "django.db.backends.mysql",
+            'NAME': "xfsuax7o44p2nqow",
+            'USER': "nrtoarmmkcqzy1za",
+            'PASSWORD': "s1qx020u1l9uaj17",
+            'PORT': "3306",
+            'HOST': "n2o93bb1bwmn0zle.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+            'CONN_MAX_AGE': 500,
+            'AUTOCOMMIT': True,
+        }
+    }
 
 if config('DEVELOP_ENV', cast=bool):
     DATABASES = {
@@ -94,20 +106,6 @@ if config('DEVELOP_ENV', cast=bool):
             'AUTOCOMMIT': True,
         }
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': "django.db.backends.mysql",
-            'NAME': "xfsuax7o44p2nqow",
-            'USER': "nrtoarmmkcqzy1za",
-            'PASSWORD': "s1qx020u1l9uaj17",
-            'PORT': "3306",
-            'HOST': "n2o93bb1bwmn0zle.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
-            'CONN_MAX_AGE': 500,
-            'AUTOCOMMIT': True,
-        }
-    }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
